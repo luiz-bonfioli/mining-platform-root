7c8354f30697a2b056fc8173cc639f54be3f3f3a
Luiz Bonfioli <luiz.bonfioli@gmail.com>
2020-08-16 11:28:55 -0300

    Security project

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,5 +3,8 @@ rootProject.name = "mining-platform-root"
 include(":mining-platform-core")
 project(":mining-platform-core").projectDir = file("../mining-platform-core")
 
+include(":mining-platform-security")
+project(":mining-platform-security").projectDir = file("../mining-platform-security")
+
 include(":mining-platform-organization")
 project(":mining-platform-organization").projectDir = file("../mining-platform-organization")
